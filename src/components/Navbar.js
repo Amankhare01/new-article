26ce675d99069b3fc832384632ab72dbda32684c
Amankhare01 <amankhare.aa@gmail.com>
2024-12-05 23:26:47 +0530

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,18 @@ function Navbar() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top">
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">NewsTak</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <button
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarSupportedContent"
+  aria-controls="navbarSupportedContent"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
+
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className='nav-item'><Link className="nav-link text-decoration-none mr-2" to="/Business">Business</Link></li>
